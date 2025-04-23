9270ca6b1821a3bcb5a9efa2c98de3be3779dfae
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-23 21:10:04 +0530

    chore: learn the hoisting

---
+++ 01_javascript/02_hoisting.js
@@ -0,0 +1,11 @@
+// console.log(a);
+var a = 5;
+// console.log(a);
+sayHello();
+function sayHello() {
+    console.log("Say Hello");
+}
+
+const sayHello2 = () => {
+    console.log("Say Hello Two");
+};
