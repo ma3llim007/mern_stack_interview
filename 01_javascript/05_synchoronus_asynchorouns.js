ae674807cda03528648a9864a584b33f4b54ae8e
ma3llim007 <mohdsameer68257@gmail.com>
2025-04-24 22:02:30 +0530

    chore: learn synchronous and asynchronous

---
+++ 01_javascript/05_synchoronus_asynchorouns.js
@@ -0,0 +1,11 @@
+// Synchronous Example:
+// console.log("1");
+// console.log("2");
+// console.log("3");
+
+// Asynchronous Example:
+console.log("1");
+setTimeout(() => {
+    console.log("2");
+}, 3000);
+console.log("3");
