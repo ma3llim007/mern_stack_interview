7d6a1e530ca954426d9d63b3ea14db1a8f975fea
ma3llim007 <mohdsameer68257@gmail.com>
2025-05-10 10:14:19 +0530

    chore: learn destructuring

---
+++ 01_javascript/23_es6_features/1_destructuring.js
@@ -0,0 +1,9 @@
+// With Array
+const [a, b] = [10, 20];
+console.log(a, b);
+
+// With Object
+const user = { name: "John", age: 20, education: "BCA" };
+const { name, age, education } = user;
+
+console.log(name,age,education);
