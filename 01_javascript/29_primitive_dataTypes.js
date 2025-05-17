0e8f83e0a18c38aa4f011f963bd8fbc169a3496f
ma3llim007 <mohdsameer68257@gmail.com>
2025-05-17 20:21:41 +0530

    chore: learn the primitive dataTypes

---
+++ 01_javascript/29_primitive_dataTypes.js
@@ -0,0 +1,25 @@
+// Primitive DataTypes
+/**
+ * 1.Number
+ * 2.String
+ * 3.Null
+ * 4.Boolean
+ * 5.Object
+ * 6.BigInt
+ * 7.symbol
+ */
+
+const num = 4;
+const str = "mes";
+const num2 = null;
+const Bool = true;
+const object = { name: "nameValue" };
+let big = 123456789012345678901234567890n;
+let id = Symbol("id")
+let id2 = Symbol("id")
+
+
+console.log(id2);
+console.log(id == id2);
+
+console.log(typeof id2);
