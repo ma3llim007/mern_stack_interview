ecbd77d7ae61803cde18fb2dfd4f774d0b400013
ma3llim007 <mohdsameer68257@gmail.com>
2025-05-18 18:41:53 +0530

    chore: learn and understand loops

---
+++ 01_javascript/32_loops/4_for_in.js
@@ -0,0 +1,5 @@
+const person = { name: "Alice", age: 25, city: "Hyderabad" };
+
+for (let key in person) {
+    console.log(key, person[key]);
+}
---
+++ 01_javascript/32_loops/5_for_of_loop.js
@@ -0,0 +1,5 @@
+const fruits = ["Apple", "Banana", "Cherry", "Dragon Fruit", "Mongo"];
+
+for (let item of fruits) {
+    console.log(item);
+}
