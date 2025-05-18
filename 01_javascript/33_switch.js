f916a6460dce2c46b85a46daa4ed4620b2d104f1
ma3llim007 <mohdsameer68257@gmail.com>
2025-05-18 19:19:04 +0530

    chore: learn switch

---
+++ 01_javascript/33_switch.js
@@ -0,0 +1,30 @@
+const dayNumber = 4;
+let day = null;
+
+switch (dayNumber) {
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    case 7:
+        day = "Sunday";
+        break;
+    default:
+        day = "Invalid day";
+}
+
+console.log(day);
