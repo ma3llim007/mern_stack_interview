ecc1b64ca00cbe2f06d10dd8f23e8502f4926bc5
ma3llim007 <mohdsameer68257@gmail.com>
2025-07-31 22:08:55 +0530

    chore: initialize project with TS

--- 5_redux/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "./features/counter.slice";
-
-const store = configureStore({
-    reducer: counterSlice.reducer,
-});
-
-export default store;
