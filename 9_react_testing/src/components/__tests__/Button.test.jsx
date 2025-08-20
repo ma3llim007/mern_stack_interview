2995b51d56fc0cc60e6768123ed235a8525baaba
ma3llim007 <mohdsameer68257@gmail.com>
2025-08-20 22:06:17 +0530

    chore: update the filename

--- 9_react_testing/src/components/__tests__/Button.test.jsx
+++ 9_react_testing/src/components/__tests__/Button.test.jsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import Button from "./Button";
+import Button from "../Button";
 
 describe.only("Button Component Test Suite", () => {
     it("should click evnet target", () => {
